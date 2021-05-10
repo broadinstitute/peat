3a1a93feed098ee7252f425dc0597cbb331f4f46
curoli <curoli@gmail.com>
2021-05-10 16:47:20 -0400

    Docker for Peat on Alpine.

---
+++ docker/alpine/gclouddock.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+project="nitrogenase-docker"
+name="peat"
+tag="1.0.0-alpine"
+full="${name}:${tag}"
+echo "Using Google project ${project}, Docker project ${name}, full tag ${full}"
+echo "Cloud-building Docker image:"
+gcloud builds submit --timeout=60m --tag gcr.io/${project}/${full}
+echo "Done"
